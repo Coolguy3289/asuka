e7b2ad4f2137544ee4d8dc5eaa6249eb42455372
Coolguy3289 <blackbirdralph@aol.com>
2019-06-27 00:24:51 -0400

    Updated README.md with config info Added ConfigLoader to read config.json file Added default.json file Implemented Logger in main class Changed main class to load config and bot token.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # asuka
 A discord entry control bot. That will email you a code based on the 
 domain provided.
+
+# Setup
+* Be sure to rename the default.json file (or copy it if updating project through git pull) to config.json, then replace the bot token in the config with the live version.
+* **Caution: Don't upload config.json to your github repo!**
--- src/main/java/me/asukabot/Asuka.java
+++ src/main/java/me/asukabot/Asuka.java
@@ -1,18 +1,26 @@
 package me.asukabot;
 
+import me.asukabot.config.Config;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.Game;
 
 import javax.security.auth.login.LoginException;
+import java.io.File;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Asuka {
     private static JDA jda;
+    private static final Logger LOGGER = LoggerFactory.getLogger(Asuka.class);
 
-    public static void main(String[] args) throws LoginException {
+
+    public static void main(String[] args) throws LoginException, IOException {
+        Config config = new Config(new File("config.json"));
         jda = new JDABuilder(AccountType.BOT)
-            .setToken("")
+            .setToken(config.getString("token"))
             .setGame(Game.playing("asukabot.com"))
             .build();
     }
